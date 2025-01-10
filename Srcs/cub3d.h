6aa5c5ecea5a5254e46706d21374e8359683eb76
Artur Silva <arturcarvalho0507@gmail.com>
2025-01-10 15:13:53 +0000

    Cleaned comments. nothing changed

--- Srcs/cub3d.h
+++ Srcs/cub3d.h
@@ -10,7 +10,9 @@
 // MINILIBX
 # include <X11/keysym.h>
 # include "../minilibx-linux/mlx.h"
+# include "../minilibx-linux/mlx_int.h"
 
+// Libft
 #include "libft.h"
 
 #define WIDTH 1920
@@ -24,11 +26,12 @@
 #define SO 3
 #define WE 4
 #define EA 5
-#define F 6 // Floor color
-#define C 7 // Ceiling color
+#define F 6 // Floor
+#define C 7 // Ceiling
 
 
-/* // Exemplos de uso:
+/* 
+Exemplos de uso:
 uint8_t  u8  = 255;       // Representante natural: unsigned char
 uint16_t u16 = 65535;     // Representante natural: unsigned short
 uint32_t u32 = 4294967295U; // Representante natural: unsigned int
@@ -37,7 +40,8 @@ uint64_t u64 = 18446744073709551615ULL; // Representante natural: unsigned long 
 int8_t   i8  = -128;      // Representante natural: signed char
 int16_t  i16 = -32768;    // Representante natural: short
 int32_t  i32 = -2147483648; // Representante natural: int
-int64_t  i64 = -9223372036854775807LL; // Representante natural: long lon */
+int64_t  i64 = -9223372036854775807LL; // Representante natural: long lon
+*/
 
 typedef struct rgb
 {
@@ -46,16 +50,20 @@ typedef struct rgb
     uint8_t b;
 }               t_rgb;
 
-
 typedef struct map_assets
 {
-    char *no_texture; //Paths
+    //Paths
+    char *no_texture;
     char *so_texture;
     char *we_texture;
     char *ea_texture;
-    char *floor_rgb_s; //Color field String
+
+    //Color field String
+    char *floor_rgb_s;
     char *ceiling_rgb_s;
-    t_rgb floor_color; //Struct cores separadas
+
+    //Struct cores separadas
+    t_rgb floor_color;
     t_rgb ceil_color;
 }               t_assets;
 
@@ -89,7 +97,7 @@ typedef struct cub
 uint8_t filetype_checker(char *path);
 uint8_t isFileValid(char *path);
 uint8_t isFileEmpty(char *path);
-uint8_t     isOrientation(char *line, t_cub *head);
+uint8_t isOrientation(char *line, t_cub *head);
 uint8_t textureValidator(t_cub *head);
 uint8_t isXpm(char *str);
 uint8_t isCharInMap(t_cub *head, char c);
@@ -99,7 +107,7 @@ uint8_t isMapclosed(t_cub *head);
 uint8_t isTherePlayer(t_cub *head);
 
 // MAP INIT
-uint8_t    getNbLines(char *path, t_cub *head);
+uint8_t getNbLines(char *path, t_cub *head);
 uint8_t allocate_map(t_cub *head);
 uint8_t collect_lines(char *path, t_cub *head);
 
