d06e9dae32e1496cd24cd20c1d84faf888135f54
Davi Melo Cardozo <dmelo-ca@c2r4s1.42porto.com>
2025-01-27 13:56:25 +0000

    Added: Floor and ceiling colors

--- code/srcs/renderer/pixel_line_builder.c
+++ code/srcs/renderer/pixel_line_builder.c
@@ -2,6 +2,12 @@
 
 void DrawVertPixelLine(t_cub *head, int color, t_ray *ray, int x) //todo change whats recieved
 {
+    // Ceiling
+    DrawLine(head->mlx, x, 0, x, ray->drawStart - 1, 0x00FFFF);
+
     // Wall 
     DrawLine(head->mlx, x, ray->drawStart, x, ray->drawEnd, color);
+
+    // Floor
+    DrawLine(head->mlx, x, ray->drawEnd + 1, x, HEIGHT - 1, 0xFFA500);
 }
