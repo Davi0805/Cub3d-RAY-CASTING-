d31560c47965ff107dd51992a89ab1bbff0384d2
Davi Melo Cardozo <dmelo-ca@c2r4s1.42porto.com>
2025-01-27 14:40:11 +0000

    Comentarios do ceiling and floor

--- code/srcs/renderer/pixel_line_builder.c
+++ code/srcs/renderer/pixel_line_builder.c
@@ -1,5 +1,8 @@
 #include "cub3d.h"
 
+
+// Converte estrutura com canais de cor separadas para um int
+// TODO: Talvez modificar para suportar transparencia
 int split_to_color(t_rgb color)
 {
     int result;
@@ -10,12 +13,12 @@ int split_to_color(t_rgb color)
 
 void DrawVertPixelLine(t_cub *head, int color, t_ray *ray, int x) //todo change whats recieved
 {
-    // Ceiling
+    // Ceiling - pinta do inicio ate 1 pixel antes da parede
     DrawLine(head->mlx, x, 0, x, ray->drawStart - 1, split_to_color(head->assets.ceil_color));
 
     // Wall 
     DrawLine(head->mlx, x, ray->drawStart, x, ray->drawEnd, color);
 
-    // Floor
+    // Floor - pinta a partir de 1 pixel depois da parede ate o limite da janela
     DrawLine(head->mlx, x, ray->drawEnd + 1, x, HEIGHT - 1, split_to_color(head->assets.floor_color));
 }
