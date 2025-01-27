380cc7d88b02b39a23a364c6d84a8a83a5f6efa5
Artur Silva <arturcarvalho0507@gmail.com>
2025-01-27 18:23:05 +0000

    time utils STILL WITH BAD CHARS

--- code/srcs/time_utils/delta_time.c
+++ code/srcs/time_utils/delta_time.c
@@ -1,11 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   delta_time.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 18:20:35 by artuda-s          #+#    #+#             */
+/*   Updated: 2025/01/27 18:22:46 by artuda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-double DeltaTime(struct timeval *old)
+double	DeltaTime(struct timeval *old)
 {
-    struct timeval new;
-    
-    gettimeofday(&new, NULL);
+	struct timeval	new;
+	double			delta_time;
 
-    double deltaTime = (new.tv_sec - old->tv_sec) + (new.tv_usec - old->tv_usec) * 1e-6;
-    return (deltaTime <= 0 ? 1/120 : deltaTime);
+	gettimeofday(&new, NULL);
+	delta_time = (new.tv_sec - old->tv_sec) + \
+				(new.tv_usec - old->tv_usec) * 1e-6;
+	if (delta_time <= 0)
+		return (1 / 120);
+	else
+		return (delta_time);
 }
