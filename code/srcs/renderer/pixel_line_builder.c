9a16c704bfffa57fd951e5f349937190c6303497
Davi Melo Cardozo <dmelo-ca@c2r4s1.42porto.com>
2025-01-27 14:06:25 +0000

    Added: Dynamic ceiling and floor color

--- code/srcs/renderer/pixel_line_builder.c
+++ code/srcs/renderer/pixel_line_builder.c
@@ -1,13 +1,21 @@
 #include "cub3d.h"
 
+int split_to_color(t_rgb color)
+{
+    int result;
+
+    result = (color.r << 16) | (color.g << 8) | color.b;
+    return result;
+}
+
 void DrawVertPixelLine(t_cub *head, int color, t_ray *ray, int x) //todo change whats recieved
 {
     // Ceiling
-    DrawLine(head->mlx, x, 0, x, ray->drawStart - 1, 0x00FFFF);
+    DrawLine(head->mlx, x, 0, x, ray->drawStart - 1, split_to_color(head->assets.ceil_color));
 
     // Wall 
     DrawLine(head->mlx, x, ray->drawStart, x, ray->drawEnd, color);
 
     // Floor
-    DrawLine(head->mlx, x, ray->drawEnd + 1, x, HEIGHT - 1, 0xFFA500);
+    DrawLine(head->mlx, x, ray->drawEnd + 1, x, HEIGHT - 1, split_to_color(head->assets.floor_color));
 }
