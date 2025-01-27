ab5c4b59e0669928d241418fcdfae9571693fb1f
Artur Silva <arturcarvalho0507@gmail.com>
2025-01-27 18:20:19 +0000

    renderer STILL WITH UPPERCASE LETTERS IN VARIABLE NAMES

--- code/srcs/renderer/pixel_line_builder.c
+++ code/srcs/renderer/pixel_line_builder.c
@@ -1,24 +1,32 @@
-#include "cub3d.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pixel_line_builder.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 18:13:43 by artuda-s          #+#    #+#             */
+/*   Updated: 2025/01/27 18:15:02 by artuda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "cub3d.h"
 
 // Converte estrutura com canais de cor separadas para um int
 // TODO: Talvez modificar para suportar transparencia
-int split_to_color(t_rgb color)
+int	split_to_color(t_rgb color)
 {
-    int result;
+	int	result;
 
-    result = (color.r << 16) | (color.g << 8) | color.b;
-    return result;
+	result = (color.r << 16) | (color.g << 8) | color.b;
+	return (result);
 }
 
-void DrawVertPixelLine(t_cub *head, int color, t_ray *ray, int x) //todo change whats recieved
+void	DrawVertPixelLine(t_cub *head, int color, t_ray *ray, int x)
 {
-    // Ceiling - pinta do inicio ate 1 pixel antes da parede
-    DrawLine(head->mlx, x, 0, x, ray->drawStart - 1, split_to_color(head->assets.ceil_color));
-
-    // Wall 
-    DrawLine(head->mlx, x, ray->drawStart, x, ray->drawEnd, color);
-
-    // Floor - pinta a partir de 1 pixel depois da parede ate o limite da janela
-    DrawLine(head->mlx, x, ray->drawEnd + 1, x, HEIGHT - 1, split_to_color(head->assets.floor_color));
+	DrawLine(head->mlx, x, 0, x, ray->drawStart - 1, \
+			split_to_color(head->assets.ceil_color));
+	DrawLine(head->mlx, x, ray->drawStart, x, ray->drawEnd, color);
+	DrawLine(head->mlx, x, ray->drawEnd + 1, x, HEIGHT - 1, \
+			split_to_color(head->assets.floor_color));
 }
--- code/srcs/renderer/ray_caster.c
+++ code/srcs/renderer/ray_caster.c
@@ -1,124 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ray_caster.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: artuda-s <artuda-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/27 18:17:17 by artuda-s          #+#    #+#             */
+/*   Updated: 2025/01/27 18:19:45 by artuda-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3d.h"
 
-static void InitRay(t_player *player, t_ray *ray, int x)
+static void	InitRay(t_player *player, t_ray *ray, int x)
 {
-    // INIT
-    ray->cameraX = 2 * x / (double)WIDTH - 1;
-    ray->DirX = player->dirX + player->planeX * ray->cameraX;
-    ray->DirY = player->dirY + player->planeY * ray->cameraX;
-    ray->mapX = (int)player->posX;
-    ray->mapY = (int)player->posY;
-    ray->deltaDistX = fabs(1 / ray->DirX);
-    ray->deltaDistY = fabs(1 / ray->DirY);
-    ray->hit = 0;
-    return ;
+	ray->cameraX = 2 * x / (double)WIDTH - 1;
+	ray->DirX = player->dirX + player->planeX * ray->cameraX;
+	ray->DirY = player->dirY + player->planeY * ray->cameraX;
+	ray->mapX = (int)player->posX;
+	ray->mapY = (int)player->posY;
+	ray->deltaDistX = fabs(1 / ray->DirX);
+	ray->deltaDistY = fabs(1 / ray->DirY);
+	ray->hit = 0;
+	return ;
 }
 
 // Calculate step and side dist
-static void GetSideDist(const t_player *player, t_ray *ray)
+static void	GetSideDist(const t_player *player, t_ray *ray)
 {
-    // X axis 0->
-    if (ray->DirX < 0)
-    {
-        ray->stepX = -1;
-        ray->sideDistX = (player->posX - ray->mapX) * ray->deltaDistX;
-    }
-    else
-    {
-        ray->stepX = 1;
-        ray->sideDistX = (ray->mapX + 1.0 - player->posX) * ray->deltaDistX;
-    }
-    // Y axis 0v
-    if (ray->DirY < 0)
-    {
-        ray->stepY = -1;
-        ray->sideDistY = (player->posY - ray->mapY) * ray->deltaDistY;
-    }
-    else
-    {
-        ray->stepY = 1;
-        ray->sideDistY = (ray->mapY + 1.0 - player->posY) * ray->deltaDistY;
-    }
-    return ;
+	if (ray->DirX < 0)
+	{
+		ray->stepX = -1;
+		ray->sideDistX = (player->posX - ray->mapX) * ray->deltaDistX;
+	}
+	else
+	{
+		ray->stepX = 1;
+		ray->sideDistX = (ray->mapX + 1.0 - player->posX) * ray->deltaDistX;
+	}
+	if (ray->DirY < 0)
+	{
+		ray->stepY = -1;
+		ray->sideDistY = (player->posY - ray->mapY) * ray->deltaDistY;
+	}
+	else
+	{
+		ray->stepY = 1;
+		ray->sideDistY = (ray->mapY + 1.0 - player->posY) * ray->deltaDistY;
+	}
+	return ;
 }
 
 // Perform DDA
-static void CastRay(t_ray *ray, char **map, t_cub *head)
+static void	CastRay(t_ray *ray, char **map, t_cub *head)
 {
-    while (true)
-    {
-        if (ray->sideDistX < ray->sideDistY)
-        {
-            ray->sideDistX += ray->deltaDistX;
-            ray->mapX += ray->stepX;
-            ray->side = 0;
-        }
-        else
-        {
-            ray->sideDistY += ray->deltaDistY;
-            ray->mapY += ray->stepY;
-            ray->side = 1;
-        }
-        if (ray->mapX < 0
-        || ray->mapY < 0
-        || ray->mapY >= head->mapHeight 
-        || ray->mapX >= head->mapLineLens[ray->mapY] 
-        || map[ray->mapY][ray->mapX] == '1' 
-        || map[ray->mapY][ray->mapX] == '\0' 
-        || map[ray->mapY][ray->mapX] == '\n')
-        {
-            ray->hit = 1;
-            return ;
-        }
-    }
+	while (true)
+	{
+		if (ray->sideDistX < ray->sideDistY)
+		{
+			ray->sideDistX += ray->deltaDistX;
+			ray->mapX += ray->stepX;
+			ray->side = 0;
+		}
+		else
+		{
+			ray->sideDistY += ray->deltaDistY;
+			ray->mapY += ray->stepY;
+			ray->side = 1;
+		}
+		if (ray->mapX < 0 || ray->mapY < 0
+			|| ray->mapY >= head->mapHeight
+			|| ray->mapX >= head->mapLineLens[ray->mapY]
+			|| map[ray->mapY][ray->mapX] == '1'
+			|| map[ray->mapY][ray->mapX] == '\0'
+			|| map[ray->mapY][ray->mapX] == '\n')
+		{
+			ray->hit = 1;
+			return ;
+		}
+	}
 }
 
 // Calculate distance and height
-static void BuildRay(t_player *player, t_ray *ray )
+static void	BuildRay(t_player *player, t_ray *ray )
 {
-    // get the Wall distance
-    if (ray->side == 0)
-        ray->perpWallDist = (ray->mapX - player->posX + (1 - ray->stepX) / 2) / ray->DirX;
-    else
-        ray->perpWallDist = (ray->mapY - player->posY + (1 - ray->stepY) / 2) / ray->DirY;
-
-    // choose a wall height
-    ray->lineHeight = (int)(HEIGHT / ray->perpWallDist);
-
-    // start of the wall
-    ray->drawStart = -ray->lineHeight / 2 + HEIGHT / 2;
-    if (ray->drawStart < 0)
-        ray->drawStart = 0;
-
-    // end of the wall
-    ray->drawEnd = ray->lineHeight / 2 + HEIGHT / 2;
-    if (ray->drawEnd >= HEIGHT)
-        ray->drawEnd = HEIGHT;
-    return ;
+	if (ray->side == 0)
+		ray->perpWallDist = (ray->mapX - player->posX + \
+							(1 - ray->stepX) / 2) / ray->DirX;
+	else
+		ray->perpWallDist = (ray->mapY - player->posY + \
+							(1 - ray->stepY) / 2) / ray->DirY;
+	ray->lineHeight = (int)(HEIGHT / ray->perpWallDist);
+	ray->drawStart = -ray->lineHeight / 2 + HEIGHT / 2;
+	if (ray->drawStart < 0)
+		ray->drawStart = 0;
+	ray->drawEnd = ray->lineHeight / 2 + HEIGHT / 2;
+	if (ray->drawEnd >= HEIGHT)
+		ray->drawEnd = HEIGHT;
+	return ;
 }
 
-int Raycaster(t_cub *head)
+int	Raycaster(t_cub *head)
 {
-    t_ray ray;
-    for (int x = 0; x < WIDTH; x++)
-    {
-        // start the current ray
-        InitRay(&head->player, &ray, x);
-        // get side distance and step based on direction
-        GetSideDist(&head->player, &ray);
-        // dda until hit a wall
-        CastRay(&ray, head->map, head);
-        // Start and end position of the wall hit
-        BuildRay(&head->player, &ray);
+	t_ray	ray;
+	int		x;
+	int		color;
 
-        //todo side colors. CHANGE THIS
-       int color = 0x0000FF;
-        if (ray.side)
-            color = 0xFF0000;
-
-        // Drawing every vertical line of pixels of the image
-        DrawVertPixelLine(head, color, &ray, x);
-    }
-    
-    return (0);
+	while (x < WIDTH)
+	{
+		InitRay(&head->player, &ray, x);
+		GetSideDist(&head->player, &ray);
+		CastRay(&ray, head->map, head);
+		BuildRay(&head->player, &ray);
+		color = 0x0000FF;
+		if (ray.side)
+			color = 0xFF0000;
+		DrawVertPixelLine(head, color, &ray, x);
+		x++;
+	}
+	return (0);
 }
